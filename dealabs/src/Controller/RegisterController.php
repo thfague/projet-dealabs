c84b5b80e1a6b2dd9894e23c685faf66b471c23f
Elsword XIII <Elsword XIII@UTILISATEUR-PC>
2020-05-28 12:05:14 +0200

    ajout authentification/création de compte (à améliorer) #4#5

---
+++ dealabs/src/Controller/RegisterController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder){
+        $form = $this->createFormBuilder()->add('pseudo')
+            ->add('mdp', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm password']
+            ])
+            ->add('register', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success float-right'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $data = $form->getData();
+            $user = new Utilisateur();
+            $user->setPseudo($data['pseudo']);
+            $user->setMdp(
+                $passwordEncoder->encodePassword($user, $data['mdp'])
+            );
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl("app_login"));
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+
+    }
+}
