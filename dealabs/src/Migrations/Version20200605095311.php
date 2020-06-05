36eb51f1266a888ff829d5b9aa982fa5309d7029
Thibault FAGUE <faguethibault@gmail.com>
2020-06-05 11:56:22 +0200

    add migration file

---
+++ dealabs/src/Migrations/Version20200605095311.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200605095311 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE deal ADD lien VARCHAR(255) DEFAULT NULL, ADD type_reduc INT DEFAULT NULL, ADD valeur_code_promo DOUBLE PRECISION DEFAULT NULL, ADD code_promo VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE deal DROP lien, DROP type_reduc, DROP valeur_code_promo, DROP code_promo');
+    }
+}
